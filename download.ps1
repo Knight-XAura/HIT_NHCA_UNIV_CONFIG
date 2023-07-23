92e4d285359cdadbed49ae90750986b7e5aecece
HPCR-SD <shawn.a.harriman@outlook.com>
2023-07-23 12:49:34 -0400

    file to send to machine to download needed files

--- download.ps1
+++ download.ps1
@@ -1,62 +1,78 @@
-﻿param (
-    [string]$repositoryUrl,
-    [string]$localDirectory
-)
+﻿# Download Locationi
+$localDirectory = Join-Path -Path C:\ -ChildPath "HIT_NHCA_UNIV_CONFIG"
+    
+while (-not $validURL) {
+    # Ask for the repository URL
+    $repoURL = Read-Host "Enter the GitHub Repository URL. (Leave blank to exit)"
+    
 
-# Validate input parameters
-if (-not $repositoryUrl -or -not $localDirectory) {
-    Write-Host "Usage: .\Download-GitHubRepo.ps1 -repositoryUrl <RepositoryURL> -localDirectory <LocalDirectory>"
-    exit 1
-}
+    if (-not $repoURL) {
+        Write-Host "Exiting the script."
+        exit 1
+    }
 
-# Extract the repository owner and name from the URL
-$regexPattern = 'github.com/(.+)/(.+?)(?:\.git)?$'
-if ($repositoryUrl -match $regexPattern) {
-    $repoOwner = $matches[1]
-    $repoName = $matches[2]
-}
-else {
-    Write-Host "Invalid GitHub repository URL."
-    exit 1
-}
+    # Extract the repository owner and name from the URL
+    $regexPattern = 'github.com/(.+)/(.+?)(?:\.git)?$'
+    if ($repoURL -match $regexPattern) {
+        $repoOwner = $matches[1]
+        $repoName = $matches[2]
+        
+    }
+    else {
+        Write-Host "Invalid GitHub Repository URL."
+        continue
+    }
+    
+    # Function to recursively download files and directories
+    function Download-RepoContents {
+        param (
+            [object]$contents,
+            [string]$basePath
+        )
+        foreach ($item in $contents) {
+            $itemUrl = $item.download_url
+            $itemPath = Join-Path -Path $basePath -ChildPath $item.name
 
-# Get the repository contents using GitHub API
-$baseUrl = "https://api.github.com/repos/$repoOwner/$repoName/contents"
-$repoContents = Invoke-RestMethod -Uri $baseUrl
+            if ($item.type -eq "dir") {
+                if (-not (Test-Path -Path $itemPath -PathType Container)) {
+                    New-Item -ItemType Directory -Path $itemPath | Out-Null
+                }
 
-# Function to recursively download files and directories
-function Download-RepoContents {
-    param (
-        [object]$contents,
-        [string]$basePath
-    )
-
-    foreach ($item in $contents) {
-        $itemUrl = $item.download_url
-        $itemPath = Join-Path -Path $basePath -ChildPath $item.name
-
-        if ($item.type -eq "dir") {
-            if (-not (Test-Path -Path $itemPath -PathType Container)) {
-                New-Item -ItemType Directory -Path $itemPath | Out-Null
+                # Recursive call to handle directory contents
+                $subContents = Invoke-RestMethod -Uri $item.url
+                Download-RepoContents -contents $subContents -basePath $itemPath
             }
-
-            # Recursive call to handle directory contents
-            $subContents = Invoke-RestMethod -Uri $item.url
-            Download-RepoContents -contents $subContents -basePath $itemPath
-        }
-        else {
-            # Download the file
-            Invoke-WebRequest -Uri $itemUrl -OutFile $itemPath
+            else {
+                # Download the file
+                Invoke-WebRequest -Uri $itemUrl -OutFile $itemPath
+            }
         }
     }
-}
+    Write-Host $isDownloaded
+    while (-not $isDownloaded) {
+        try {
+            Write-Host "Downloading repository contents..."
+            $repoContents = Invoke-RestMethod -Uri "https://api.github.com/repos/$repoOwner/$repoName/contents"
+            New-Item -ItemType Directory -Path $localDirectory | Out-Null
+            Download-RepoContents -contents $repoContents -basePath $localDirectory 
+            Write-Host "Repository downloaded successfully to: $localDirectory"
+            $validURL = $true
+            $isDownloaded = $true
+        }
+        catch {
+            Write-Host "Failed to download repository contents. Error: $_"
+            $choice = Read-Host "Do you want to retry? (Yes/No)"
+            if ($choice -ne "Yes") {
+                $isDownloaded = $true
+            }
 
-# Start downloading the repository contents
-try {
-    Write-Host "Downloading repository contents..."
-    Download-RepoContents -contents $repoContents -basePath $localDirectory
-    Write-Host "Repository downloaded successfully to: $localDirectory"
-}
-catch {
-    Write-Host "Failed to download repository contents. Error: $_"
+            else {
+                if (Test-Path -Path $localDirectory -PathType Container) {
+                    Remove-Item -Path $localDirectory -Recurse -Force
+                }
+                $isDownloaded = $false
+                $repoURL = $null
+            }
+        }
+    }
 }
